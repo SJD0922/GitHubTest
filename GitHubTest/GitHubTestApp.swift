788c0b67f1e78023739447c67610f36c72c91ec1
SJD0922 <stephendavis346@gmail.com>
2021-11-29 13:10:10 -0600

    Initial Commit

---
+++ GitHubTest/GitHubTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubTestApp.swift
+//  GitHubTest
+//
+//  Created by PM HTML5 - Stevie Davis  on 11/29/21.
+//
+
+import SwiftUI
+
+@main
+struct GitHubTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
